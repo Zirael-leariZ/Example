8681f3be11e6efff7e0a2b00c0ab1889243a9f0b
Oleksii Ishchenko <optionalzirelleariz>
2023-08-06 00:04:30 -0600

    sum with Randoms

---
+++ example005RandomSum/Program.cs
@@ -0,0 +1,19 @@
+﻿int min;
+int max;
+
+Console.WriteLine ("What's min: ");
+min = int.Parse(Console.ReadLine());
+
+Console.WriteLine ("What's max: ");
+max = int.Parse(Console.ReadLine());
+
+Random random = new Random();
+int A = random.Next(min,max);
+int B = random.Next(min,max);
+
+Console.WriteLine("The min is: " + A);
+Console.WriteLine("The max is : " + B);
+
+Console.WriteLine ("A + B is: " + (A + B));
+
+Console.ReadLine();
