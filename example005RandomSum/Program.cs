0652b1c9e603e6737dc57d03b2bdadeffc449d0b
Oleksii Ishchenko <optionalzirelleariz>
2023-08-06 00:22:55 -0600

    compare exercise

--- example005RandomSum/Program.cs
+++ example005RandomSum/Program.cs
@@ -11,8 +11,8 @@ Random random = new Random();
 int A = random.Next(min,max);
 int B = random.Next(min,max);
 
-Console.WriteLine("The min is: " + A);
-Console.WriteLine("The max is : " + B);
+Console.WriteLine("The A is: " + A);
+Console.WriteLine("The B is : " + B);
 
 Console.WriteLine ("A + B is: " + (A + B));
 
